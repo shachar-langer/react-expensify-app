bfe8fe11659acc27da9abc065c269b0d5bde03ad
Shachar Langer <shachar.langer@gmail.com>
2019-12-06 16:47:42 +0200

    Bug Fix - displayed quarter instead of day

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -11,7 +11,7 @@ const ExpenseListItem = (props) => (
         <p>
             {numeral(props.amount / 100).format('$0,0.00')}
             -
-            {moment(props.createdAt).format('MMMM Qo, YYYY')}
+            {moment(props.createdAt).format('MMMM Do, YYYY')}
         </p>
     </div>
 )
